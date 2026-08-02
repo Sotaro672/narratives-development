a5e7a2b57b56792fc6824ceee86ee6ad4f40758b
sotaro <caotailangaogang@gmail.com>
2026-08-02 18:16:44 +0900

    make feature directory

--- frontend/amol/src/features/wallet/types.ts
+++ frontend/amol/src/features/wallet/types.ts
@@ -1,11 +1,4 @@
 // frontend/amol/src/features/wallet/types.ts
-export type WalletAvatar = {
-  avatarId: string;
-  avatarName: string;
-  avatarIcon: string;
-  profile: string;
-};
-
 export type WalletTabKey =
   | "history"
   | "tokens"
