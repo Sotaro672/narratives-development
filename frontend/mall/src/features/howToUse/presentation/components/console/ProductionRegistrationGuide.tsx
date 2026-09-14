dad0f7b299a3ff066e31f5c5ef9ffb8af0104970
sotaro <caotailangaogang@gmail.com>
2026-09-15 08:45:47 +0900

    refactoring according to backend BFF

--- frontend/mall/src/features/howToUse/presentation/components/console/ProductionRegistrationGuide.tsx
+++ frontend/mall/src/features/howToUse/presentation/components/console/ProductionRegistrationGuide.tsx
@@ -1,5 +1,6 @@
 // frontend/mall/src/features/howToUse/presentation/components/console/ProductionRegistrationGuide.tsx
 
+import enterProductionQuantityVideo from "../../../assets/console/production-registration/enter-production-quantity.mp4";
 import goToProductionRegistrationVideo from "../../../assets/console/production-registration/go-to-production-registration.mp4";
 
 import HowToUseNote from "../common/HowToUseNote";
@@ -23,15 +24,19 @@ export default function ProductionRegistrationGuide() {
         />
       </HowToUseSection>
 
-      <HowToUseSection title="生産計画作成手順">
+      <HowToUseSection title="生産数の入力">
         <HowToUseStepList>
+          <HowToUseStep>生産計画の担当者を選択してください。</HowToUseStep>
           <HowToUseStep>生産する商品のブランドを選択してください。</HowToUseStep>
           <HowToUseStep>商品設計一覧から、生産する商品を選択してください。</HowToUseStep>
-          <HowToUseStep>選択した商品設計の内容を確認してください。</HowToUseStep>
           <HowToUseStep>「モデル別 生産数一覧」で、各モデルの生産数を入力してください。</HowToUseStep>
-          <HowToUseStep>生産計画の担当者を選択してください。</HowToUseStep>
           <HowToUseStep>すべての入力が完了したら、画面上部の「保存」をクリックしてください。</HowToUseStep>
         </HowToUseStepList>
+
+        <HowToUseVideo
+          src={enterProductionQuantityVideo}
+          label="ブランドと商品設計を選択し、モデル別生産数一覧で各モデルの生産数を入力する手順"
+        />
 
         <HowToUseNote title="商品設計">
           生産計画を作成するには、あらかじめ対象商品の商品設計を登録しておく必要があります。ブランドを選択すると、そのブランドの商品設計から生産対象を選択できます。
