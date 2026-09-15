5914b1ceabd13bf4bb58e758b6810d7fe0ed59f0
sotaro <caotailangaogang@gmail.com>
2026-09-15 13:20:05 +0900

    refactoring according to backend BFF

--- frontend/mall/src/features/howToUse/presentation/components/console/ListGuide.tsx
+++ frontend/mall/src/features/howToUse/presentation/components/console/ListGuide.tsx
@@ -2,6 +2,7 @@
 
 import createListVideo from "../../../assets/console/list/create-list.mp4";
 import setLocationAndTransportationVideo from "../../../assets/console/list/set-location-and-transportation.mp4";
+import stopListingVideo from "../../../assets/console/list/stop-listing.mp4";
 
 import HowToUseSection from "../common/HowToUseSection";
 import HowToUseStep from "../common/HowToUseStep";
@@ -11,7 +12,7 @@ import HowToUseVideo from "../common/HowToUseVideo";
 export default function ListGuide() {
   return (
     <>
-      <HowToUseSection title="出品設定">
+      <HowToUseSection title="在庫の保管場所と配送料金を設定">
         <HowToUseStepList>
           <HowToUseStep>サイドバーから「商品」「在庫」を開いてください。</HowToUseStep>
           <HowToUseStep>出品する在庫行を選択してください。</HowToUseStep>
@@ -23,13 +24,23 @@ export default function ListGuide() {
         <HowToUseVideo src={setLocationAndTransportationVideo} label="出品する商品に在庫保管場所と配送料金体系を設定する手順" />
       </HowToUseSection>
 
-      <HowToUseSection title="出品情報の作成">
+      <HowToUseSection title="出品作成">
         <HowToUseStepList>
           <HowToUseStep>商品画像と販売価格を入力してください。</HowToUseStep>
           <HowToUseStep>画面右上の作成ボタンを押してください。</HowToUseStep>
         </HowToUseStepList>
 
         <HowToUseVideo src={createListVideo} label="商品の出品情報を入力し、購入者Mallへの出品を作成する手順" />
+      </HowToUseSection>
+
+      <HowToUseSection title="出品停止">
+        <HowToUseStepList>
+          <HowToUseStep>出品一覧から停止する出品を開いてください。</HowToUseStep>
+          <HowToUseStep>出品詳細画面から出品停止を実行してください。</HowToUseStep>
+          <HowToUseStep>出品を停止すると、購入者Mallでの販売が停止されます。</HowToUseStep>
+        </HowToUseStepList>
+
+        <HowToUseVideo src={stopListingVideo} label="出品中の商品を停止し、購入者Mallでの販売を停止する手順" />
       </HowToUseSection>
     </>
   );
