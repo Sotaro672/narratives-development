c872a1afa8fe687f312e9477c9e695d8c8457503
sotaro <caotailangaogang@gmail.com>
2026-09-15 10:09:33 +0900

    refactoring according to backend BFF

--- frontend/mall/src/features/howToUse/presentation/components/console/InspectionGuide.tsx
+++ frontend/mall/src/features/howToUse/presentation/components/console/InspectionGuide.tsx
@@ -30,16 +30,12 @@ export default function InspectionGuide() {
 
           <HowToUseStepList>
             <HowToUseStep>検品スキャナーで商品のQRコードをスキャンしてください。</HowToUseStep>
-            <HowToUseStep>表示された商品設計とモデル情報を確認し、実際の商品と一致していることを確認してください。</HowToUseStep>
             <HowToUseStep>検品結果として「合格」「不合格」「未製造」のいずれかを選択してください。</HowToUseStep>
-            <HowToUseStep>続けて別の商品を検品する場合は「検品を続ける」を押してスキャン画面へ戻り、次の商品のQRコードをスキャンしてください。</HowToUseStep>
-            <HowToUseStep>対象となる商品の検品がすべて完了したら「検品を完了する」を押してください。</HowToUseStep>
+            <HowToUseStep>続けて別の商品を検品する場合はヘッダーの戻るアイコンを押してください。</HowToUseStep>
+            <HowToUseStep>ミント申請画面で検品完了ボタンを押すと、未検査の商品はすべて「合格」として登録されます。</HowToUseStep>
           </HowToUseStepList>
         </div>
-
-        <HowToUseNote title="検品結果">「合格」は商品が登録内容と一致し問題がない場合、「不合格」は商品に問題がある場合、「未製造」は生産予定に含まれているものの実際には製造されなかった場合に選択してください。</HowToUseNote>
-
-        <HowToUseNote title="検品スキャナー">検品スキャナーは商品のQRコードをカメラで読み取って使用するモバイル専用アプリです。QRコードを読み取れる端末から操作してください。</HowToUseNote>
+        <HowToUseNote title="検品スキャナー">この検品はネガティブ登録制です。「不合格」「未製造」の商品IDをミント対象から弾くことを目的とします。</HowToUseNote>
       </HowToUseSection>
     </>
   );
