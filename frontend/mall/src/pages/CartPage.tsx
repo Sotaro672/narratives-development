0071a0fc6d69f2cbace4200d0f165b9fb9472076
sotaro <caotailangaogang@gmail.com>
2026-09-26 14:04:18 +0900

    refactoring according to backend BFF

--- frontend/mall/src/pages/CartPage.tsx
+++ frontend/mall/src/pages/CartPage.tsx
@@ -49,8 +49,7 @@ export default function CartPage() {
 
   return (
     <Layout
-      title="カート"
-      titleClickable={false}
+      title="AMOL"
       mode="mypage"
       showFooter={isMobilePortrait}
       hideHamburgerMenu
